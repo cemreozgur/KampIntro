f85e609ab355c78dcd64de8da6626bea73316de1
CİHAN <CHAN@ALI-CIHAN>
2021-01-26 23:26:01 +0300

    21,01,21

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Cemre","Ali","Cihan","Mine"};
+            //System.Console.WriteLine(isimler[0]);
+            //System.Console.WriteLine(isimler[1]);
+            //System.Console.WriteLine(isimler[2]);
+            //System.Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];
+            //isimler[4] = "Mete";
+            //System.Console.WriteLine(isimler[4]);
+            //System.Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Cemre", "Ali", "Cihan", "Mine" };
+
+            System.Console.WriteLine(isimler2[0]);
+            System.Console.WriteLine(isimler2[1]);
+            System.Console.WriteLine(isimler2[2]);
+            System.Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            System.Console.WriteLine(isimler2[4]);
+            System.Console.WriteLine(isimler2[0]);
+
+            
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Cemre";
+            musteri1.Soyadi = "Özgür";
+            musteri1.Id = 1;
+            musteri1.TcNo = "4343434";
+            musteri1.MusteriNo = "2323232";
+
+        }
+    }
+}
